c912cfc2d4ff43cddc37bd1ad772602c97d7264a
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2000-03-21 19:46:15 +0000

    added setup.py
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@36 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ setup.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup (name = "linkchecker",
+       version = "1.1.2",
+       description = "check links of HTML pages",
+       author = "Bastian Kleineidam",
+       author_email = "calvin@users.sourceforge.net",
+       url = "http://linkchecker.sourceforge.net",
+       licence = "GPL",
+
+       packages = ['','DNS','linkcheck','distutils','distutils/command'],
+       # uncomment ext_modules to enable HTTPS support
+       # you must have an SSL library and the Python header
+       # files installed
+       #ext_modules = [('ssl', {'sources': ['ssl.c'],
+       #                 'include_dirs': ['/usr/include/openssl'],
+       #                 'libs': ['ssl']})],
+       scripts = [('linkchecker.bat', 'nt')],
+       programs = ['linkchecker'],
+       )
