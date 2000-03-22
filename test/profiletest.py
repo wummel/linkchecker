7cf789a68e0ddf6315906a1fff6712f90a6bf213
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2000-03-22 01:15:55 +0000

    move some files
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@41 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- test/profiletest.py
+++ test/profiletest.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 # profiling test
+import sys
+sys.path.append("/home/calvin/projects/linkchecker/")
 import linkcheck,re,profile,pstats
+url="http://treasure.calvinsplayground.de/~calvin/"
 config = linkcheck.Config.Configuration()
-config.appendUrl(linkcheck.UrlData.GetUrlDataFrom("http://www.yahoo.de/", 0))
+config["verbose"] = 1
+config.appendUrl(linkcheck.UrlData.GetUrlDataFrom(url, 0))
 profile.run("linkcheck.checkUrls(config)", "test.prof")
-
 p = pstats.Stats("test.prof")
 p.strip_dirs().sort_stats("time").print_stats(10)
