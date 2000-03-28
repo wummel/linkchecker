38701affef16eabc1e10980af07977b852d99528
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2000-03-28 14:54:31 +0000

    Install instructions
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@49 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkchecker.bat
+++ linkchecker.bat
@@ -1,4 +1,4 @@
 @echo off
 
 rem Limited to 9 parameters? Is there a $* for Windows?
-python "@INSTALL_BIN@\linkchecker" %1 %2 %3 %4 %5 %6 %7 %8 %9
+python "c:\progra~1\linkchecker-1.1.3\linkchecker" %1 %2 %3 %4 %5 %6 %7 %8 %9
