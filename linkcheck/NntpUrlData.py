58ec06b11972699037cd93f8c8ceee8c2cb3d1b9
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2000-03-30 18:04:21 +0000

    added NntpUrlData.py
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@53 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ linkcheck/NntpUrlData.py
@@ -0,0 +1,32 @@
+import nntplib,re,string
+from HostCheckingUrlData import HostCheckingUrlData
+from UrlData import LinkCheckerException
+
+nntp_re =  re.compile("^news:[\w.\-]+$")
+
+class NntpUrlData(HostCheckingUrlData):
+    "Url link with NNTP scheme"
+    
+    def buildUrl(self):
+        HostCheckingUrlData.buildUrl(self)
+        if not nntp_re.match(self.urlName):
+            raise LinkCheckerException, "Illegal NNTP link syntax"
+        self.host = string.lower(self.urlName[5:])
+
+
+    def checkConnection(self, config):
+        if not config["nntpserver"]:
+            self.setWarning("No NNTP server specified, checked only syntax")
+        nntp = nntplib.NNTP(config["nntpserver"])
+        resp,count,first,last,name = nntp.group(self.host)
+        self.setInfo("Group %s has %s articles, range %s to %s" % \
+                     (name, count, first, last))
+
+
+    def getCacheKey(self):
+        return "news:"+HostCheckingUrlData.getCacheKey(self)
+
+
+    def __str__(self):
+        return "NNTP link\n"+HostCheckingUrlData.__str__(self)
+
