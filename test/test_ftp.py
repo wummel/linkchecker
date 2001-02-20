c64918774b2e578b7ee88b52e232118441e62b78
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2001-02-20 23:58:35 +0000

    added test suite files
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@234 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ test/test_ftp.py
@@ -0,0 +1,14 @@
+import os,sys
+sys.path.append(os.getcwd())
+import linkcheck
+config = linkcheck.Config.Configuration()
+config['recursionlevel'] = 1
+config['log'] = config.newLogger('test')
+config["anchors"] = 1
+config["verbose"] = 1
+config.disableThreading()
+htmldir = "test/html"
+for file in ('ftp.html'):
+    url = os.path.join(htmldir, file)
+    config.appendUrl(linkcheck.UrlData.GetUrlDataFrom(url, 0))
+linkcheck.checkUrls(config)
