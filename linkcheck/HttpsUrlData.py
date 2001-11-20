8bbe7db6df939bfd120de446b7f9843d2c2b3667
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2001-11-20 20:41:07 +0000

    fix test cases
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@320 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/HttpsUrlData.py
+++ linkcheck/HttpsUrlData.py
@@ -15,6 +15,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
+import httplib
 from UrlData import UrlData
 from HttpUrlData import HttpUrlData
 from linkcheck import _, Config
--- test/regrtest.py
+++ test/regrtest.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python2
+#!/usr/bin/env python
 
 # this file is _not_ the original Python2 regression test suite.
 
