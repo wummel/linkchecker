af9cd2d82f8a41c12a1c2f80776620dd43518492
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2002-01-28 21:24:59 +0000

    fix telnet
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@357 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/TelnetUrlData.py
+++ linkcheck/TelnetUrlData.py
@@ -20,19 +20,42 @@ from HostCheckingUrlData import HostCheckingUrlData
 from linkcheck import _
 
 # regular expression for syntax checking
-telnet_re =  re.compile("^telnet:[\w.\-]+$")
+
+_user = "[-a-zA-Z0-9$_.+!*'()]+"
+_password = _user
+_userpassword = r"((?P<user1>%s)|(?P<user>%s):(?P<password>%s))@" % \
+                (_user, _user, _password)
+_label = r"[a-zA-Z][-a-zA-Z0-9]*"
+_host = r"%s(\.%s)*| " % (_label, _label)
+_port = r"\d+"
+telnet_re =  re.compile(r"^telnet://(%s)?(?P<host>%s)(:(?P<port>%s))?(/)?$"%\
+                        (_userpassword, _host, _port))
 
 class TelnetUrlData(HostCheckingUrlData):
     "Url link with telnet scheme"
 
     def buildUrl(self):
         HostCheckingUrlData.buildUrl(self)
-        if not telnet_re.match(self.urlName):
+        mo = telnet_re.match(self.urlName)
+        if not mo:
             raise linkcheck.error, _("Illegal telnet link syntax")
-        self.host = self.urlName[7:].lower()
+        self.user = mo.group("user")
+        self.password = mo.group("password")
+        self.host = mo.group("host")
+        self.port = mo.group("port")
+        if not self.port:
+            self.port = 23
 
 
     def checkConnection(self, config):
         HostCheckingUrlData.checkConnection(self, config)
         self.urlConnection = telnetlib.Telnet()
-        self.urlConnection.open(self.host, 23)
+        self.urlConnection.open(self.host, self.port)
+        if self.user:
+            self.urlConnection.read_until("login: ", 10)
+            self.urlConnection.write(self.user+"\n")
+            if self.password:
+                self.urlConnection.read_until("Password: ", 10)
+                self.urlConnection.write(self.password+"\n")
+        self.urlConnection.write("exit\n")
+
--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ myname = "Bastian Kleineidam"
 myemail = "calvin@users.sourceforge.net"
 
 setup (name = "linkchecker",
-       version = "1.3.16",
+       version = "1.3.17",
        description = "check HTML documents for broken links",
        author = myname,
        author_email = myemail,
