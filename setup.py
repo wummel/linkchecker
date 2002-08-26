508e39c451156381884b1679ee8ebae8fdf243d4
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2002-08-26 22:19:17 +0000

    python2.1->2.2 switch
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@496 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- setup.py
+++ setup.py
@@ -129,7 +129,7 @@ myname = "Bastian Kleineidam"
 myemail = "calvin@users.sourceforge.net"
 
 setup (name = "linkchecker",
-       version = "1.6.0",
+       version = "1.6.1",
        description = "check HTML documents for broken links",
        author = myname,
        author_email = myemail,
