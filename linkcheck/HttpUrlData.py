22291c1c0ced7d59a769b13ca5d7c5a0a33d80da
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2002-09-26 15:08:37 +0000

    add info about proxy usage
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@549 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/HttpUrlData.py
+++ linkcheck/HttpUrlData.py
@@ -212,6 +212,7 @@ class HttpUrlData (UrlData):
 	"""
         if self.proxy:
             host = self.proxy
+            self.setInfo(_("Using Proxy %s")%self.proxy)
         else:
             host = self.urlTuple[1]
         Config.debug(HURT_ME_PLENTY, "host", host)
