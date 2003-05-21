6f1f665b1cb6c8a78cf04583db5b666dc2eb51d9
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2003-05-21 22:31:02 +0000

    updated
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@893 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- test/test_misc.py
+++ test/test_misc.py
@@ -7,7 +7,7 @@ config["anchors"] = 1
 config["verbose"] = 1
 config.disableThreading()
 htmldir = "test/html"
-for file in ('misc.html',):
+for file in ('misc.html','anchor.html'):
     url = os.path.join(htmldir, file)
     config.appendUrl(linkcheck.UrlData.GetUrlDataFrom(url, 0, config))
 linkcheck.checkUrls(config)
