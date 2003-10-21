3a973eb1a9c03fbbfc1d10c3d59db7ff513ce21b
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2003-10-21 21:56:02 +0000

    oops
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1064 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/HttpUrlData.py
+++ linkcheck/HttpUrlData.py
@@ -16,7 +16,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 
-import urlparse, sys, time, re, httplib, zlib, gzip robotparser2
+import urlparse, sys, time, re, httplib, zlib, gzip, robotparser2
 from urllib import quote, unquote
 from cStringIO import StringIO
 import Config, i18n
