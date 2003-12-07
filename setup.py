da72bf35e8c50c31a82c1214927c9bfd8e5a4d05
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2003-12-07 10:47:40 +0000

    bump up version
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1093 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- setup.py
+++ setup.py
@@ -146,7 +146,7 @@ myname = "Bastian Kleineidam"
 myemail = "calvin@users.sourceforge.net"
 
 setup (name = "linkchecker",
-       version = "1.9.5",
+       version = "1.10.0",
        description = "check HTML documents for broken links",
        author = myname,
        author_email = myemail,
