48498d967b7e50726f19e8301bc4b4abb1b9dd1b
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2004-01-29 19:07:35 +0000

    docs
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1227 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck-cron.sh
+++ linkcheck-cron.sh
@@ -3,10 +3,13 @@
 # adjust as you wish
 # GNU GPL Copyright (C) 2004  Bastian Kleineidam
 
+# replace this with your url(s) you want to check separated by whitespace
+# urls can also be local files, eg /var/www/index.html
+URL="http://www.imadoofus.org/"
+
 PATH="/bin:/usr/bin"
 LOGFILE="$HOME/.linkchecker_blacklist"
-URL="http://www.heise.de"
 
 ./linkchecker -Fblacklist/$LOGFILE $URL
-
+# this awk script complains if urls fail for at least two days
 [ -r $LOGFILE ] && awk '/^[[:digit:]]+/ {if ($1 > 1) printf "URL %s failed for %d days.", $2, $1; }' $LOGFILE
