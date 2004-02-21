3a3c50c7dce10a3592a5813e3fc97937c34d3a2c
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2004-02-21 11:57:12 +0000

    added
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1262 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ test/get.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+# -*- coding: iso-8859-1 -*-
+"""print contents of an url"""
+
+import httplib, urlparse, sys
+
+def main (url):
+    parts = urlparse.urlsplit(url)
+    host = parts[1]
+    path = urlparse.urlunsplit(('', '', parts[2], parts[3], parts[4]))
+    h = httplib.HTTPConnection(host)
+    h.connect()
+    h.putrequest("GET", path, skip_host=True)
+    h.putheader("Host", host)
+    h.putheader("Accept-Encoding", "gzip;q=1.0, deflate;q=0.9, identity;q=0.5")
+    h.endheaders()
+    req = h.getresponse()
+    print req.read()
+
+
+if __name__=='__main__':
+    main(sys.argv[1])
