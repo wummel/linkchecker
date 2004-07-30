b48c84fe219a49599f97f42076f61860c196045a
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2004-07-30 08:11:40 +0000

    removed
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1404 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- bk/net/dns/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: iso-8859-1 -*-
-#
-# This file is part of the pydns project.
-# Homepage: http://pydns.sourceforge.net
-#
-# This code is covered by the standard Python License.
-#
-"""Python DNS module"""
-
