311be4ac04a341e14d26e99843fd4ab8dee400dc
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2004-08-23 22:53:27 +0000

    added
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1465 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ linkcheck/tests/test_urlbuild.py
@@ -0,0 +1,43 @@
+# -*- coding: iso-8859-1 -*-
+"""test url build method from url data objects"""
+# Copyright (C) 2004  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+
+import unittest
+import linkcheck.checker.httpurl
+
+class TestUrlBuild (unittest.TestCase):
+    """test url building"""
+
+    def test_http_build (self):
+        parent_url = "http://localhost:8001/linkcheck/ftests/data/http.html"
+        base_url = "http://"
+        recursion_level = 0
+        consumer = None
+        o = linkcheck.checker.httpurl.HttpUrl(base_url, recursion_level,
+               consumer, parent_url=parent_url)
+        o.build_url()
+        self.assertEquals(o.url, 'http:///')
+
+
+def test_suite ():
+    """build and return a TestSuite"""
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestUrlBuild))
+    return suite
+
+if __name__ == '__main__':
+    unittest.main()
