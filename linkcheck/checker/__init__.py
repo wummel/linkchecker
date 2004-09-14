fda2392fc635c27c4e2eee7bc1b68f70e94ec7d5
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2004-09-14 20:01:35 +0000

    do not catch cookie error on check
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@1802 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/checker/__init__.py
+++ linkcheck/checker/__init__.py
@@ -24,7 +24,6 @@ import re
 import urlparse
 import nntplib
 import ftplib
-import Cookie
 
 import linkcheck.httplib2
 import linkcheck.dns.exception
@@ -54,8 +53,6 @@ ExcList = [
     ftplib.error_temp,
     ftplib.error_perm,
     ftplib.error_proto,
-    # Cookie error
-    Cookie.CookieError,
 ]
 # XXX remove this when depending on python >= 2.4
 if hasattr(socket, "sslerror"):
