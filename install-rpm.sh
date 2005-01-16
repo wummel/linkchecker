8d8bfbc315c41a6ba9b4d2719663b6c85b743d59
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-01-16 23:36:07 +0000

    support bzip compresses man pages
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2144 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- install-rpm.sh
+++ install-rpm.sh
@@ -1,5 +1,11 @@
 python setup.py install --root=$RPM_BUILD_ROOT --record=INSTALLED_FILES
-# 'brp-compress' gzips the man pages without distutils knowing... fix this
-sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
-sed -i -e 's@man/de/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/de/man\1/\2.gz@g' INSTALLED_FILES
-sed -i -e 's@man/fr/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/fr/man\1/\2.gz@g' INSTALLED_FILES
+# 'brp-compress' compresses the man pages without distutils knowing...
+if [ -n "$( ls $RPM_BUILD_ROOT/usr/share/man/man*/*.bz2 2>/dev/null )" ]; then
+    sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.bz2@g' INSTALLED_FILES
+    sed -i -e 's@man/de/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/de/man\1/\2.bz2@g' INSTALLED_FILES
+    sed -i -e 's@man/fr/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/fr/man\1/\2.bz2@g' INSTALLED_FILES
+else
+    sed -i -e 's@man/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/man\1/\2.gz@g' INSTALLED_FILES
+    sed -i -e 's@man/de/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/de/man\1/\2.gz@g' INSTALLED_FILES
+    sed -i -e 's@man/fr/man\([[:digit:]]\)/\(.\+\.[[:digit:]]\)$@man/fr/man\1/\2.gz@g' INSTALLED_FILES
+fi
