e58c070df79d993c520149c39e0cb3026c525e7d
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-01-18 14:45:51 +0000

    supply Py_CLEAR macro
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2149 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/HtmlParser/htmlsax.h
+++ linkcheck/HtmlParser/htmlsax.h
@@ -28,9 +28,21 @@
 #error please install Python >= 2.3
 #endif
 
-/* this will be in Python 2.4 */
+/* Py_RETURN_NONE is in Python 2.4 */
 #ifndef Py_RETURN_NONE
 #define Py_RETURN_NONE do {Py_INCREF(Py_None); return Py_None;} while (0)
+#endif
+
+/* Py_CLEAR is in Python 2.4 */
+#ifndef Py_CLEAR
+#define Py_CLEAR(op)                            \
+        do {                                    \
+                if (op) {                       \
+                        PyObject *tmp = (PyObject *)(op);       \
+                        (op) = NULL;            \
+                        Py_DECREF(tmp);         \
+                }                               \
+        } while (0)
 #endif
 
 /* user_data type for SAX calls */
