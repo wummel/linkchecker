5fb817be93f696e3250b10b09f563f0728a23039
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-01-24 19:27:00 +0000

    require python 2.4
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2205 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/__init__.py
+++ linkcheck/__init__.py
@@ -21,8 +21,8 @@ Main function module for link checking.
 # imports and checks
 import sys
 if not hasattr(sys, 'version_info') or \
-   sys.version_info < (2, 3, 0, 'final', 0):
-    raise SystemExit, "This program requires Python 2.3 or later."
+   sys.version_info < (2, 4, 0, 'final', 0):
+    raise SystemExit, "This program requires Python 2.4 or later."
 import os
 import re
 
