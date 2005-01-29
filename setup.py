35971fa7dd4f6dcc3a385c6a95481936bb8781d3
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-01-29 23:17:06 +0000

    2.3 version
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2240 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- setup.py
+++ setup.py
@@ -270,7 +270,7 @@ elif os.name == 'nt':
               'doc/shot2_thumb.jpg', ]))
 
 setup (name = "linkchecker",
-       version = "3.0",
+       version = "2.3",
        description = "check HTML documents for broken links",
        keywords = "link,url,checking,verfication",
        author = myname,
