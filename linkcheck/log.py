6919a06d8cc61296f66a58068dc883ed056ba6ed
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-05-06 22:17:23 +0000

    fix backtrace printing, and only call _log when the loglevel is enabled
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2573 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/log.py
+++ linkcheck/log.py
@@ -23,7 +23,7 @@ __all__ = ["debug", "info", "warn", "error", "critical", "exception",
            "trace", "is_debug"]
 
 import logging
-import traceback
+import os
 import inspect
 import cStringIO as StringIO
 import linecache
@@ -36,14 +36,14 @@ import sys
 
 # call tracing
 
-tracelog = ""
-
+tracelog = None
 def trace (log):
     """
     Start tracing of the current thread (and the current thread only).
     """
     global tracelog
-    tracelog = log
+    if tracelog is None:
+        tracelog = log
     sys.settrace(_traceit)
 
 
@@ -58,7 +58,7 @@ def _traceit (frame, event, arg):
             filename = filename[:-1]
         name = frame.f_globals["__name__"]
         line = linecache.getline(filename, lineno)
-        debug(tracelog, "%s:%s: %s", name, lineno, line.rstrip())
+        info(tracelog, "%s:%s: %s", name, lineno, line.rstrip())
     return _traceit
 
 
@@ -71,23 +71,21 @@ def _stack_format (stack):
     @rtype: string
     """
     s = StringIO.StringIO()
-    traceback.print_stack(stack, file=s)
-    if PRINT_LOCALVARS:
-        s.write("Locals by frame, innermost last%s" % os.linesep)
-        for frame in stack:
-            s.write(os.linesep)
-            s.write("Frame %s in %s at line %s%s" % (frame.f_code.co_name,
-                                                     frame.f_code.co_filename,
-                                                     frame.f_lineno,
-                                                     os.linesep))
+    s.write('Traceback:')
+    s.write(os.linesep)
+    for frame, fname, lineno, method, lines, i  in reversed(stack):
+        s.write('  File %r, line %d, in %s' % (fname, lineno, method))
+        s.write(os.linesep)
+        s.write('    %s' % lines[0].lstrip())
+        if PRINT_LOCALVARS:
             for key, value in frame.f_locals.items():
-                s.write("\t%20s = " % key)
+                s.write("      %s = " % key)
                 # be careful not to cause a new error in the error output
                 try:
-                    s.write(str(value))
+                    s.write(repr(value))
                     s.write(os.linesep)
                 except:
-                    s.write("error in str() call%s" % os.linesep)
+                    s.write("error in repr() call%s" % os.linesep)
     return s.getvalue()
 
 
@@ -100,66 +98,77 @@ def _log (fun, msg, args, tb=False):
     fun(msg, *args)
     if tb:
         # note: get rid of last parts of the stack
-        s = _stack_format(inspect.stack()[2:])
-        fun(s)
+        fun(_stack_format(inspect.stack()[2:]))
 
 
-def debug (log, msg, *args, **kwargs):
+def debug (logname, msg, *args, **kwargs):
     """
     Log a debug message.
 
     return: None
     """
-    _log(logging.getLogger(log).debug, msg, args, tb=kwargs.get("tb"))
+    log = logging.getLogger(logname)
+    if log.isEnabledFor(logging.DEBUG):
+        _log(log.debug, msg, args, tb=kwargs.get("tb"))
 
 
-def info (log, msg, *args, **kwargs):
+def info (logname, msg, *args, **kwargs):
     """
     Log an informational message.
 
     return: None
     """
-    _log(logging.getLogger(log).info, msg, args, tb=kwargs.get("tb"))
+    log = logging.getLogger(logname)
+    if log.isEnabledFor(logging.INFO):
+        _log(log.info, msg, args, tb=kwargs.get("tb"))
 
 
-def warn (log, msg, *args, **kwargs):
+def warn (logname, msg, *args, **kwargs):
     """
     Log a warning.
 
     return: None
     """
-    _log(logging.getLogger(log).warn, msg, args, tb=kwargs.get("tb"))
+    log = logging.getLogger(logname)
+    if log.isEnabledFor(logging.WARN):
+        _log(log.warn, msg, args, tb=kwargs.get("tb"))
 
 
-def error (log, msg, *args, **kwargs):
+def error (logname, msg, *args, **kwargs):
     """
     Log an error.
 
     return: None
     """
-    _log(logging.getLogger(log).error, msg, args, tb=kwargs.get("tb"))
+    log = logging.getLogger(logname)
+    if log.isEnabledFor(logging.ERROR):
+        _log(log.error, msg, args, tb=kwargs.get("tb"))
 
 
-def critical (log, msg, *args, **kwargs):
+def critical (logname, msg, *args, **kwargs):
     """
     Log a critical error.
 
     return: None
     """
-    _log(logging.getLogger(log).critical, msg, args, tb=kwargs.get("tb"))
+    log = logging.getLogger(logname)
+    if log.isEnabledFor(logging.CRITICAL):
+        _log(log.critical, msg, args, tb=kwargs.get("tb"))
 
 
-def exception (log, msg, *args, **kwargs):
+def exception (logname, msg, *args, **kwargs):
     """
     Log an exception.
 
     return: None
     """
-    _log(logging.getLogger(log).exception, msg, args, tb=kwargs.get("tb"))
+    log = logging.getLogger(logname)
+    if log.isEnabledFor(logging.EXCEPTION):
+        _log(log.exception, msg, args, tb=kwargs.get("tb"))
 
 
-def is_debug (log):
+def is_debug (logname):
     """
     See if logger is on debug level.
     """
-    return logging.getLogger(log).isEnabledFor(logging.DEBUG)
+    return logging.getLogger(logname).isEnabledFor(logging.DEBUG)
