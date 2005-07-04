3cec6b2bc000de2f5d03646d8bc383e69e0dad66
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-07-04 15:34:21 +0000

    moved comment
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2699 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- bdist.bat
+++ bdist.bat
@@ -1,4 +1,4 @@
-REM batch file for generating the windows .exe installer
 @echo off
+REM batch file for generating the windows .exe installer
 c:\Programme\Python24\python.exe setup.py sdist --manifest-only
 c:\Programme\Python24\python.exe setup.py build -c mingw32 bdist_wininst -b hase.bmp
