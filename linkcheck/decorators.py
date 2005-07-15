e2e80367d1a86766f16b8f19752f67e127d2ec78
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-07-15 21:41:54 +0000

    add copyright, add more documentation
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2762 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/decorators.py
+++ linkcheck/decorators.py
@@ -1,5 +1,34 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
 """
 Simple decorators (usable in Python >= 2.4).
+
+Example:
+
+@synchronized(thread.allocate_lock())
+def f ():
+    "Synchronized function"
+    print "i am synchronized:", f, f.__doc__
+
+@deprecated
+def g ():
+    "this function is deprecated"
+    pass
+
 """
 import warnings
 import signal
@@ -73,15 +102,3 @@ def synchronized (lock):
         return _synchronized(lock, function)
     return newfunc
 
-
-if __name__ == '__main__':
-    import thread
-    @synchronized(thread.allocate_lock())
-    def f ():
-        """Just me"""
-        print "i am synchronized:", f, f.__doc__
-    f()
-    @deprecated
-    def g ():
-        pass
-    g()
