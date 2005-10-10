9c0ef1396d19c6f439ed576a6bff01b9e385025d
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-10-10 21:15:37 +0000

    syntax
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2820 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- doc/bfknav.py
+++ doc/bfknav.py
@@ -14,7 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-"""General navigation writer reading .nav file info"""
+"""
+General navigation writer reading .nav file info.
+"""
 
 import sys
 import os
