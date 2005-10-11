5f5712c3917277c6759aaf55e63d11b534a21206
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-10-11 15:31:41 +0000

    add .git repository dir to exclude list
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2854 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- test.py
+++ test.py
@@ -217,7 +217,7 @@ def get_test_files(cfg):
                 remove.append(idx)
             elif '.' in file and os.path.isdir(os.path.join(dir, file)):
                 remove.append(idx)
-            elif file in ["CVS", ".svn"]:
+            elif file in ["CVS", ".svn", ".git"]:
                 remove.append(idx)
         remove.reverse()
         for idx in remove:
