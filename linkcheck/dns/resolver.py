b48c6f7e96f0de9ca5a19c4f0132451d2b5cc1b3
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-10-11 09:55:26 +0000

    fix default resolver init
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2843 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/dns/resolver.py
+++ linkcheck/dns/resolver.py
@@ -685,6 +685,7 @@ def get_default_resolver ():
     global default_resolver
     if default_resolver is None:
         default_resolver = Resolver()
+    return default_resolver
 
 def query(qname, rdtype=linkcheck.dns.rdatatype.A, rdclass=linkcheck.dns.rdataclass.IN,
           tcp=False, resolver=None):
