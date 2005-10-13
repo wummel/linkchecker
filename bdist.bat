ab2a968b1e26c045526c31fc5fe5beec3f5faa8f
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-10-13 12:14:31 +0000

    add icon editor run
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2863 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- bdist.bat
+++ bdist.bat
@@ -3,3 +3,5 @@ REM batch file for generating the windows .exe installer
 set PYTHON=c:\Python24\python.exe
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b hase.bmp
+REM start resource editor for .exe icon
+"%PROGRAMFILES%\PE Resource Explorer\PEResourceExplorer.exe"
