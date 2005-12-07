89defcab9968f35b7ba0736b42ff79d5f2742a51
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-12-07 21:48:30 +0000

    moved
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2947 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- test/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-# run python interpreter with current dir as search path, and remove all
-# locale and proxy settings
-PYVER=2.4
-env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C PYTHONPATH=`pwd` python${PYVER} "$@"
