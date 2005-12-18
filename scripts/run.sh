b1c335e2aebbbafcbadecafbd612f19c3f6bfa74
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2005-12-18 09:14:41 +0000

    add pyopts
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@2990 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/run.sh
+++ scripts/run.sh
@@ -2,4 +2,6 @@
 # run python interpreter with current dir as search path, and remove all
 # locale and proxy settings
 PYVER=2.4
-env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C PYTHONPATH=`pwd` python${PYVER} "$@"
+PYOPTS=
+#PYOPTS=-O
+env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C PYTHONPATH=`pwd` python${PYVER} $PYOPTS "$@"
