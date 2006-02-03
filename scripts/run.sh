a58135882c9232bdb4dd34454c4ebf6eb0cbfb80
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-02-03 22:14:48 +0000

    cleanup
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3053 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-# run python interpreter with current dir as search path, and remove all
-# locale and proxy settings
+# Run python interpreter with current dir as search path, and remove all
+# locale and proxy settings.
+# Set PYOPTS for options, for example PYOPTS=-O.
 PYVER=2.4
-PYOPTS=
-#PYOPTS=-O
-env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C PYTHONPATH=`pwd` python${PYVER} $PYOPTS "$@"
+env -u ftp_proxy -u http_proxy -u LANGUAGE -u LC_ALL -u LC_CTYPE LANG=C PYTHONPATH=`pwd` python${PYVER} ${PYOPTS:-} "$@"
