ab0ad2e9efc8a232dd2f02e5043f0d7bd774bd97
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-02-13 17:48:07 +0000

    bump up version
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3069 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- setup.py
+++ setup.py
@@ -502,7 +502,7 @@ elif win_compiling:
              ]))
 
 setup (name = "linkchecker",
-       version = "3.4",
+       version = "3.5",
        description = "check websites and HTML documents for broken links",
        keywords = "link,url,checking,verification",
        author = myname,
