54921be131c0e3efc5672e8254c5b10248e141ad
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-02-13 17:50:28 +0000

    add space in python path
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3074 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- bdist.bat
+++ bdist.bat
@@ -1,6 +1,6 @@
 @echo off
 REM batch file for generating the windows .exe installer
-set PYTHON="c:\Python24\python.exe"
+set PYTHON="c:\Python 24\python.exe"
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b hase.bmp
 REM start resource editor for .exe icon
