acc72824e5b70a5e2165c45d2023fef308f94126
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-04-03 23:11:11 +0000

    add clean target, xn resource editor
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3108 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- bdist.bat
+++ bdist.bat
@@ -1,7 +1,8 @@
 @echo off
 REM batch file for generating the windows .exe installer
 set PYTHON="c:\Python 24\python.exe"
+%PYTHON% setup.py clean --all
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b hase.bmp
 REM start resource editor for .exe icon
-"%PROGRAMFILES%\PE Resource Explorer\PEResourceExplorer.exe"
+"%PROGRAMFILES%\XN Resource Editor\XNResourceEditor.exe"
