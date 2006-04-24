db1f9948ed92c6bd7c4348a2b3cecbb4450c62f7
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-04-24 20:11:48 +0000

    StandardTest moved into test.py
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3125 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- tests/__init__.py
+++ tests/__init__.py
@@ -20,39 +20,6 @@ Test utilities.
 import unittest
 
 
-class StandardTest (unittest.TestCase):
-    """
-    A test case with improved inequality test and resource support.
-    """
-
-    def setUp (self):
-        """
-        Check resources, using the provided function check_resources()
-        from test.py.
-        """
-        super(StandardTest, self).setUp()
-        if hasattr(self, "needed_resources"):
-            self.check_resources(self.needed_resources)
-
-    def failUnlessEqual (self, first, second, msg=None):
-        """
-        Define the first argument as the test value, and the second
-        one as the excpected value. Adjust the default error message
-        accordingly.
-        """
-        if msg is None:
-            r1 = repr(first)
-            r2 = repr(second)
-            if len(r1) > 40 or len(r2) > 40:
-                sep = "\n"
-            else:
-                sep = ", "
-            msg = "got %s%sexpected %s" % (r1, sep, r2)
-        super(StandardTest, self).failUnlessEqual(first, second, msg=msg)
-
-    assertEqual = assertEquals = failUnlessEqual
-
-
 def make_suite (prefix, namespace):
     """
     Add all TestCase classes starting with given prefix to a test suite.
