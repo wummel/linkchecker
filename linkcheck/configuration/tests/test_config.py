ac9d88cd996c5a71618f870fb5c11fedc083db6e
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-05-13 09:51:50 +0000

    added config test
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3141 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ linkcheck/configuration/tests/test_config.py
@@ -0,0 +1,56 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2006 Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+Test config parsing.
+"""
+
+import unittest
+import os
+import linkcheck.configuration
+
+
+def get_file (filename):
+    """
+    Get file name located within 'data' directory.
+    """
+    return unicode(os.path.join("linkcheck", "configuration", "tests",
+                                "data", filename))
+
+
+class TestConfig (unittest.TestCase):
+    """
+    Test cgi routines.
+    """
+
+    def test_confparse (self):
+        """
+        Check url validity.
+        """
+        config = linkcheck.configuration.Configuration()
+        files = [get_file("config1.ini")]
+        config.read(files)
+
+
+def test_suite ():
+    """
+    Build and return a TestSuite.
+    """
+    return unittest.makeSuite(TestConfig)
+
+
+if __name__ == '__main__':
+    unittest.main()
