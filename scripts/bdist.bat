ecb945b61b3dd967fe7c51826a752cdf97f44287
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-05-19 19:25:07 +0000

    update docs for dist build
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3279 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/bdist.bat
+++ scripts/bdist.bat
@@ -1,6 +1,13 @@
 @echo off
 REM batch file for generating the windows .exe installer
 set PYTHON="c:\Python 24\python.exe"
+cd doc\en
+%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time documentation.txt documentation.html
+%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time index.txt index.html
+%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time install.txt install.html
+%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time other.txt other.html
+%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time upgrading.txt upgrading.html
+cd ..\..
 %PYTHON% setup.py clean --all
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32 bdist_wininst -b hase.bmp
