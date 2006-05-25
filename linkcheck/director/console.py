cb853c4672cd03b54145f1a9ce93ad474cd9e03d
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-05-25 11:32:32 +0000

    documentation
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3312 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- linkcheck/director/console.py
+++ linkcheck/director/console.py
@@ -14,6 +14,9 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+Helpers for console output.
+"""
 import sys
 import os
 import codecs
