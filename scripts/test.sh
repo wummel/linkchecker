23446620b2d5cc183bd5b37beb0864d8e6c22961
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-06-05 19:43:32 +0000

    remove unwanted coverage files
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3346 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/test.sh
+++ scripts/test.sh
@@ -6,6 +6,16 @@ if [ $# -eq 0 ]; then
 fi
 $P/run.sh test.py $RESOURCES $coverage -pvcw "$@"
 if [ $# -eq 0 ]; then
+    # remove files with 100% coverage
     rm `grep -L '>>>>>>' coverage/*.cover`
+    # don't care for these ones
     rm coverage/*tests.*.cover
+    rm coverage/linkcheck.dns.*.cover
+    rm coverage/linkcheck.httplib2.cover
+    rm coverage/linkcheck.gzip2.cover
+    # don't know why these are not filtered
+    rm coverage/encodings.*.cover
+    rm coverage/email.*.cover
+    rm coverage/logging.*.cover
+    rm coverage/xml.*.cover
 fi
