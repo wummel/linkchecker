1ea71455b43a86dc3a6d20d14016bf3d52d74b0d
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-06-06 06:56:23 +0000

    remove more unwanted coverage files
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3366 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/test.sh
+++ scripts/test.sh
@@ -9,13 +9,15 @@ if [ $# -eq 0 ]; then
     # remove files with 100% coverage
     rm `grep -L '>>>>>>' coverage/*.cover`
     # don't care for these ones
-    rm coverage/*tests.*.cover
-    rm coverage/linkcheck.dns.*.cover
-    rm coverage/linkcheck.httplib2.cover
-    rm coverage/linkcheck.gzip2.cover
+    rm coverage/*tests.*.cover || true
+    rm coverage/linkcheck.dns.*.cover || true
+    rm coverage/linkcheck.httplib2.cover || true
+    rm coverage/linkcheck.gzip2.cover || true
     # don't know why these are not filtered
-    rm coverage/encodings.*.cover
-    rm coverage/email.*.cover
-    rm coverage/logging.*.cover
-    rm coverage/xml.*.cover
+    rm coverage/_xmlplus.*.cover || true
+    rm coverage/encodings.*.cover || true
+    rm coverage/email.*.cover || true
+    rm coverage/logging.*.cover || true
+    rm coverage/psyco.*.cover || true
+    rm coverage/xml.*.cover || true
 fi
