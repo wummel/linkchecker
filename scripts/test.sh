4238cb9f59dec1f7e99ef6687da1156d58ee4795
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-06-06 18:20:40 +0000

    use exit code of test run, not -e option
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3368 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,23 +1,25 @@
-#!/bin/sh -e
+#!/bin/sh
 P=`dirname $0`
 RESOURCES=`$P/resources.sh`
 if [ $# -eq 0 ]; then
     coverage="--coverage"
 fi
 $P/run.sh test.py $RESOURCES $coverage -pvcw "$@"
+res=$?
 if [ $# -eq 0 ]; then
     # remove files with 100% coverage
     rm `grep -L '>>>>>>' coverage/*.cover`
     # don't care for these ones
-    rm coverage/*tests.*.cover || true
-    rm coverage/linkcheck.dns.*.cover || true
-    rm coverage/linkcheck.httplib2.cover || true
-    rm coverage/linkcheck.gzip2.cover || true
+    rm coverage/*tests.*.cover
+    rm coverage/linkcheck.dns.*.cover
+    rm coverage/linkcheck.httplib2.cover
+    rm coverage/linkcheck.gzip2.cover
     # don't know why these are not filtered
-    rm coverage/_xmlplus.*.cover || true
-    rm coverage/encodings.*.cover || true
-    rm coverage/email.*.cover || true
-    rm coverage/logging.*.cover || true
-    rm coverage/psyco.*.cover || true
-    rm coverage/xml.*.cover || true
+    rm coverage/_xmlplus.*.cover
+    rm coverage/encodings.*.cover
+    rm coverage/email.*.cover
+    rm coverage/logging.*.cover
+    rm coverage/psyco.*.cover
+    rm coverage/xml.*.cover
 fi
+exit $res
