69c92d84b65ada73fa5fd4c372f34851a7259edb
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-11-21 17:46:25 +0000

    added examples
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3519 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ doc/examples/check_for_x_errors.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+# This script is in the public domain.
+# Author of this script is Daniel Webb
+# Modified by Bastian Kleineidam:
+# - added hash-bang first line
+# - documentation
+# - removed second function, run them commands as-is
+#
+# Check web site links once per day, report only when the check had more
+# than X errors.
+#   Return 0
+# arguments:
+#   $1 - web site URL
+#   $2 - notification email
+#   $3 - threshold number of errors
+#
+function die() { echo "$0: $*"; exit 1; }
+
+logfile=/tmp/linkchecker.log
+[ -z "$1" -o -z "$2" -o -z "$3" ] && die "check_web_links requires three arguments"
+do_check=false
+if [ ! -f $logfile ]; then
+    do_check=true
+else
+    # Has it been at least a day since last check?
+    find $logfile -mtime +1 | grep link && do_check=true
+fi
+if [ $do_check = true ]; then
+    linkchecker $1 >$logfile 2>/dev/null
+    errors=$(grep Error: $logfile | wc -l)
+    if [ $errors -gt $3 ]; then
+        cat $logfile | mail -s "linkchecker: more than $3 errors" $2
+    fi
+fi
+return 0
---
+++ doc/examples/check_urls.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+# Copyright (C) 2004-2006  Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# Script suitable for cron job URL checking
+# Usage:
+# check_urls.sh [--cron] [linkchecker options] [urls...]
+# 
+# And with crontab -e you add for example the following entry:
+#
+#    # check my site each night
+#    10 4 * * * $HOME/bin/check_urls.sh --cron http://mysite.com/
+#
+# To only get a mail when errors are encountered, you have to disable
+# the intro and outro output in a config file $HOME/.linkchecker/cron:
+#
+#    [text]
+#    parts=realurl,result,extern,base,name,parenturl,info,warning,url
+#
+
+
+
+if which linkchecker > /dev/null; then
+    LC=linkchecker
+else
+    echo "linkchecker binary not found"
+    exit 1
+fi
+LCOPTS="-f$HOME/.linkchecker/cron"
+if [ "$1" = "--cron" ]; then
+    shift
+    LCOPTS="$LCOPTS --no-status"
+    D=/dev/null
+else
+    D=/dev/stdout
+fi
+echo "Begin checking..." > $D
+$LC $LCOPTS "$@"
