0c1e9196bd28f833a2501460ef337a26962064a5
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2006-11-21 18:05:32 +0000

    documentation
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3521 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- doc/examples/check_blacklist.sh
+++ doc/examples/check_blacklist.sh
@@ -1,14 +1,23 @@
 #!/bin/sh
-# this script is intended to be run daily from cron
-# GNU GPL Copyright (C) 2004-2006 Bastian Kleineidam
-
-# Put here your url(s) you want to check, separated by whitespace.
-# Urls can also be local files, eg "/var/www/index.html".
-URL="http://www.imadoofus.org/"
-
-PATH="/bin:/usr/bin"
+# Copyright (C) 2004-2006 Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+#
+# This script is intended to be run daily from cron. It complains when
+# URLs fail for at least a number of script runs.
 LOGFILE="$HOME/.linkchecker/blacklist"
-
-linkchecker -Fblacklist $URL
+linkchecker -Fblacklist "$@"
 # this awk script complains if urls fail for at least two script runs
 [ -r $LOGFILE ] && awk '/^[[:digit:]]+/ {if ($1 > 1) printf "URL %s failed for %d days.", $2, $1; }' $LOGFILE
