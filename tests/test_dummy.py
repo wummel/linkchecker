61147a455e5c462a9698f2215fec1956d224ef9e
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2008-05-20 17:02:13 +0000

    Resurrect test_dummy!
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3768 e7d03fd6-7b0d-0410-9947-9c21f3af8025

---
+++ tests/test_dummy.py
@@ -0,0 +1,75 @@
+# -*- coding: iso-8859-1 -*-
+# Copyright (C) 2005-2008 Bastian Kleineidam
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+"""
+Test dummy object.
+"""
+
+import unittest
+import linkcheck.strformat
+import linkcheck.dummy
+
+
+class TestDummy (unittest.TestCase):
+    """
+    Test dummy object.
+    """
+
+    def test_creation (self):
+        dummy = linkcheck.dummy.Dummy()
+        dummy = linkcheck.dummy.Dummy("1")
+        dummy = linkcheck.dummy.Dummy("1", "2")
+        dummy = linkcheck.dummy.Dummy(a=1, b=2)
+        dummy = linkcheck.dummy.Dummy("1", a=None, b=2)
+
+    def test_attributes (self):
+        dummy = linkcheck.dummy.Dummy()
+        dummy.hulla
+        dummy.hulla.bulla
+        dummy.hulla = 1
+        del dummy.wulla
+        del dummy.wulla.mulla
+
+    def test_methods (self):
+        dummy = linkcheck.dummy.Dummy()
+        dummy.hulla()
+        dummy.hulla().bulla()
+        if "a" in dummy:
+            pass
+
+    def test_indexes (self):
+        dummy = linkcheck.dummy.Dummy()
+        i = len(dummy)
+        dummy[1] = dummy[2]
+        dummy[1][-1]
+        dummy[1:3] = None
+        del dummy[1]
+        del dummy[2]
+        del dummy[2:3]
+        s = str(dummy)
+        s = repr(dummy)
+        s = unicode(dummy)
+
+
+def test_suite ():
+    """
+    Build and return a TestSuite.
+    """
+    return unittest.makeSuite(TestDummy)
+
+
+if __name__ == '__main__':
+    unittest.main()
