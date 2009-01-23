e2a303b727fd25d21316e8ab71725ba8ba9f9a41
calvin <calvin@e7d03fd6-7b0d-0410-9947-9c21f3af8025>
2009-01-23 09:15:48 +0000

    Remove old doc generation from batch file.
    git-svn-id: https://linkchecker.svn.sourceforge.net/svnroot/linkchecker/trunk/linkchecker@3921 e7d03fd6-7b0d-0410-9947-9c21f3af8025

--- scripts/bdist.bat
+++ scripts/bdist.bat
@@ -1,14 +1,6 @@
 @echo off
 : batch file for generating the windows .exe installer
 set PYTHON="c:\Python26\python.exe"
-cd doc\en
-%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time documentation.txt documentation.html
-%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time index.txt index.html
-%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time install.txt install.html
-%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time other.txt other.html
-%PYTHON% ..\rest2htmlnav --stylesheet-path=lc.css --time upgrading.txt upgrading.html
-%PYTHON% ..\bfknav.py
-cd ..\..
 rd /S /Q build
 rd /S /Q dist
 %PYTHON% setup.py sdist --manifest-only
