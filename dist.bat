5bf592f9c14588a0413d546fa79a10802e8b4aa3
Bastian Kleineidam <calvin@debian.org>
2009-02-24 11:56:25 +0100

    Added build.bat batch file

--- dist.bat
+++ dist.bat
@@ -1,7 +1,6 @@
 @echo off
 : batch file for generating the windows .exe installer
 set PYTHON="c:\Python26\python.exe"
-rd /S /Q build
+call build.bat
 rd /S /Q dist
-%PYTHON% setup.py sdist --manifest-only
-%PYTHON% setup.py build -c mingw32 py2exe
+%PYTHON% setup.py py2exe
--- test.bat
+++ test.bat
@@ -1,3 +1,4 @@
 @echo off
-c:\python26\python.exe -3 c:\python26\scripts\nosetests -m "^test_.*" tests/
+set PYTHON="c:\Python26\python.exe"
+%PYTHON% -3 c:\python26\scripts\nosetests -m "^test_.*" tests/
 @pause
