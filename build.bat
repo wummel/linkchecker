4ecf4248c87c83239ea9e31c25fa815c64712fdd
Bastian Kleineidam <calvin@debian.org>
2009-02-24 16:32:04 +0100

    Added pause to dist

--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
 @echo off
 set PYTHON="c:\Python26\python.exe"
-rd /S /Q build
 %PYTHON% setup.py sdist --manifest-only
 %PYTHON% setup.py build -c mingw32
 copy build\lib.win32-2.6\linkcheck\HtmlParser\htmlsax.pyd linkcheck\HtmlParser
--- dist.bat
+++ dist.bat
@@ -1,6 +1,8 @@
 @echo off
 : batch file for generating the windows .exe installer
 set PYTHON="c:\Python26\python.exe"
+rd /S /Q build
 call build.bat
 rd /S /Q dist
 %PYTHON% setup.py py2exe
+@pause
