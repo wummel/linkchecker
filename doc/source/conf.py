59352537f5219f4f2debaa4fdda4340653aa242e
Bastian Kleineidam <calvin@debian.org>
2009-03-04 20:14:31 +0100

    Remove underscore from static and template paths

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -29,7 +29,7 @@
 extensions = []
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.txt'
@@ -115,7 +115,7 @@ html_favicon = "favicon.ico"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ['static']
 
 # If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
 # using the given strftime format.
