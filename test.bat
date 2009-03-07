1b1e4bea1f7382139c4ce2cf9bbe3f401762dccd
Bastian Kleineidam <calvin@debian.org>
2009-03-07 16:04:38 +0100

    Run nosetest -v on windows

--- test.bat
+++ test.bat
@@ -1,4 +1,4 @@
 @echo off
 set PYTHON="c:\Python26\python.exe"
-%PYTHON% -3 c:\python26\scripts\nosetests -m "^test_.*" tests/
+%PYTHON% -3 c:\python26\scripts\nosetests -v -m "^test_.*" tests/
 @pause
