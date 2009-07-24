5e06b6b8d425b80317454fe032b7594af4472396
Bastian Kleineidam <calvin@debian.org>
2009-07-24 23:58:20 +0200

    Updated FSF address in GPL blurb

--- linkcheck/cookies.py
+++ linkcheck/cookies.py
@@ -11,9 +11,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 Parsing and storing of cookies. See [1]RFC 2965 and [2]RFC 2109.
 The reason for this module is that neither the cookielib nor the Cookie
--- linkcheck/logger/dot.py
+++ linkcheck/logger/dot.py
@@ -11,9 +11,9 @@
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
 A DOT graph format logger. The specification has been taken from
 http://www.graphviz.org/doc/info/lang.html
