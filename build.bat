1a3f9d6fbac18c55ed4f03851c0304412f9c8f35
Bastian Kleineidam <calvin@debian.org>
2009-07-31 22:06:47 +0200

    Updated clean and build batch files

--- build.bat
+++ build.bat
@@ -4,3 +4,4 @@ set PYTHON="c:\Python26\python.exe"
 %PYTHON% setup.py build -c mingw32
 copy build\lib.win32-2.6\linkcheck\HtmlParser\htmlsax.pyd linkcheck\HtmlParser
 copy build\lib.win32-2.6\linkcheck\network\_network.pyd linkcheck\network
+e:\qt\qt\bin\qcollectiongenerator doc\html\lccollection.qhcp -o doc\html\lccollection.qhc
--- clean.bat
+++ clean.bat
@@ -1,3 +1,7 @@
 @echo off
 set PYTHON="c:\Python26\python.exe"
 %PYTHON% setup.py clean --all
+del linkcheck\HtmlParser\htmlsax.pyd
+del linkcheck\network\_network.pyd
+del doc\html\lccollection.qhc
+del doc\html\lcdoc.qch
