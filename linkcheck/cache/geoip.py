8995be17394be4c280e93909d65cc833f497530e
Bastian Kleineidam <calvin@debian.org>
2010-09-29 14:10:36 +0200

    Support city-level geoip lookup; remove the geoip cache since lookup is fast enough; remove the duplicated geoip country name map.

--- linkcheck/cache/geoip.py
+++ linkcheck/cache/geoip.py
@@ -18,305 +18,58 @@
 Store and retrieve country names for IPs.
 """
 import os
+import sys
 from ..lock import get_lock
 from ..containers import LFUCache
 from ..decorators import synchronized
 
-# I don't know if the geoip library is already thread-safe, but
-# we take no risks here.
+# It is unknown if the geoip library is already thread-safe, so
+# no risks should be taken here by using a lock.
 _lock = get_lock("geoip")
 
-# initialize GeoIP database
-if os.name = 'nt':
-    import sys
-    geoip_dat = os.path.join(sys.exec_prefix, "GeoIP.dat")
-else:
-    geoip_dat = "/usr/share/GeoIP/GeoIP.dat"
+def get_geoip_dat ():
+    """Find a GeoIP database, preferring city over country lookup."""
+    datafiles = ("GeoIPCity.dat", "GeoIP.dat")
+    if os.name == 'nt':
+        paths = (sys.exec_prefix, r"c:\geoip")
+    else:
+        paths = ("/usr/local/share/GeoIP", "/usr/share/GeoIP")
+    for path in paths:
+        for datafile in datafiles:
+            filename = os.path.join(path, datafile)
+            if os.path.exists(filename):
+                return filename
+
 # try importing both the C-library GeoIP and the pure-python pygeoip
+geoip_dat = get_geoip_dat()
 geoip = None
-try:
-    import GeoIP
-    if os.name == 'posix' and os.path.exists(geoip_dat):
-        geoip = GeoIP.open(geoip_dat, GeoIP.GEOIP_STANDARD)
-except ImportError:
+if geoip_dat:
     try:
-        import pygeoip
-        if os.path.exists(geoip_dat):
-            geoip = pygeoip.GeoIP(geoip_dat)
+        import GeoIP
+        geoip = GeoIP.open(geoip_dat, GeoIP.GEOIP_STANDARD)
     except ImportError:
-        pass
-del geoip_dat
+        try:
+            import pygeoip
+            geoip = pygeoip.GeoIP(geoip_dat)
+        except ImportError:
+            pass
 
-
-country_cache = LFUCache(size=1000)
 
 @synchronized(_lock)
 def get_country (host):
     """Get translated country name.
 
-    @return: country string or None
+    @return: country with optional city or an empty string if not found
     """
     if geoip is None:
         # no geoip available
         return None
-    if host in country_cache:
-        value = country_cache[host]
-    else:
-        c = geoip.country_code_by_name(host)
-        if c and c in countries:
-            value = "%s, %s" % (c, countries[c])
-        else:
-            value = None
-        country_cache[host] = value
+    record = geoip.record_by_name(host)
+    value = ""
+    if "city" in record:
+        value = record["city"]
+    if "country_name" in record:
+        if value:
+            value += ", "
+        value += record["country_name"]
     return value
-
-
-# GeoIP country map with {short name -> translated full name} entries
-countries = {
-    "AP": "Asia/Pacific Region",
-    "EU": "Europe",
-    "AD": "Andorra",
-    "AE": "United Arab Emirates",
-    "AF": "Afghanistan",
-    "AG": "Antigua and Barbuda",
-    "AI": "Anguilla",
-    "AL": "Albania",
-    "AM": "Armenia",
-    "AN": "Netherlands Antilles",
-    "AO": "Angola",
-    "AQ": "Antarctica",
-    "AR": "Argentina",
-    "AS": "American Samoa",
-    "AT": "Austria",
-    "AU": "Australia",
-    "AW": "Aruba",
-    "AZ": "Azerbaijan",
-    "BA": "Bosnia and Herzegovina",
-    "BB": "Barbados",
-    "BD": "Bangladesh",
-    "BE": "Belgium",
-    "BF": "Burkina Faso",
-    "BG": "Bulgaria",
-    "BH": "Bahrain",
-    "BI": "Burundi",
-    "BJ": "Benin",
-    "BM": "Bermuda",
-    "BN": "Brunei Darussalam",
-    "BO": "Bolivia",
-    "BR": "Brazil",
-    "BS": "Bahamas",
-    "BT": "Bhutan",
-    "BV": "Bouvet Island",
-    "BW": "Botswana",
-    "BY": "Belarus",
-    "BZ": "Belize",
-    "CA": "Canada",
-    "CC": "Cocos (Keeling) Islands",
-    "CD": "Congo, The Democratic Republic of the",
-    "CF": "Central African Republic",
-    "CG": "Congo",
-    "CH": "Switzerland",
-    "CI": "Cote D'Ivoire",
-    "CK": "Cook Islands",
-    "CL": "Chile",
-    "CM": "Cameroon",
-    "CN": "China",
-    "CO": "Colombia",
-    "CR": "Costa Rica",
-    "CU": "Cuba",
-    "CV": "Cape Verde",
-    "CX": "Christmas Island",
-    "CY": "Cyprus",
-    "CZ": "Czech Republic",
-    "DE": "Germany",
-    "DJ": "Djibouti",
-    "DK": "Denmark",
-    "DM": "Dominica",
-    "DO": "Dominican Republic",
-    "DZ": "Algeria",
-    "EC": "Ecuador",
-    "EE": "Estonia",
-    "EG": "Egypt",
-    "EH": "Western Sahara",
-    "ER": "Eritrea",
-    "ES": "Spain",
-    "ET": "Ethiopia",
-    "FI": "Finland",
-    "FJ": "Fiji",
-    "FK": "Falkland Islands (Malvinas)",
-    "FM": "Micronesia, Federated States of",
-    "FO": "Faroe Islands",
-    "FR": "France",
-    "FX": "France, Metropolitan",
-    "GA": "Gabon",
-    "GB": "United Kingdom",
-    "GD": "Grenada",
-    "GE": "Georgia",
-    "GF": "French Guiana",
-    "GH": "Ghana",
-    "GI": "Gibraltar",
-    "GL": "Greenland",
-    "GM": "Gambia",
-    "GN": "Guinea",
-    "GP": "Guadeloupe",
-    "GQ": "Equatorial Guinea",
-    "GR": "Greece",
-    "GS": "South Georgia and the South Sandwich Islands",
-    "GT": "Guatemala",
-    "GU": "Guam",
-    "GW": "Guinea-Bissau",
-    "GY": "Guyana",
-    "HK": "Hong Kong",
-    "HM": "Heard Island and McDonald Islands",
-    "HN": "Honduras",
-    "HR": "Croatia",
-    "HT": "Haiti",
-    "HU": "Hungary",
-    "ID": "Indonesia",
-    "IE": "Ireland",
-    "IL": "Israel",
-    "IN": "India",
-    "IO": "British Indian Ocean Territory",
-    "IQ": "Iraq",
-    "IR": "Iran, Islamic Republic of",
-    "IS": "Iceland",
-    "IT": "Italy",
-    "JM": "Jamaica",
-    "JO": "Jordan",
-    "JP": "Japan",
-    "KE": "Kenya",
-    "KG": "Kyrgyzstan",
-    "KH": "Cambodia",
-    "KI": "Kiribati",
-    "KM": "Comoros",
-    "KN": "Saint Kitts and Nevis",
-    "KP": "Korea, Democratic People's Republic of",
-    "KR": "Korea, Republic of",
-    "KW": "Kuwait",
-    "KY": "Cayman Islands",
-    "KZ": "Kazakhstan",
-    "LA": "Lao People's Democratic Republic",
-    "LB": "Lebanon",
-    "LC": "Saint Lucia",
-    "LI": "Liechtenstein",
-    "LK": "Sri Lanka",
-    "LR": "Liberia",
-    "LS": "Lesotho",
-    "LT": "Lithuania",
-    "LU": "Luxembourg",
-    "LV": "Latvia",
-    "LY": "Libyan Arab Jamahiriya",
-    "MA": "Morocco",
-    "MC": "Monaco",
-    "MD": "Moldova, Republic of",
-    "MG": "Madagascar",
-    "MH": "Marshall Islands",
-    "MK": "Macedonia",
-    "ML": "Mali",
-    "MM": "Myanmar",
-    "MN": "Mongolia",
-    "MO": "Macau",
-    "MP": "Northern Mariana Islands",
-    "MQ": "Martinique",
-    "MR": "Mauritania",
-    "MS": "Montserrat",
-    "MT": "Malta",
-    "MU": "Mauritius",
-    "MV": "Maldives",
-    "MW": "Malawi",
-    "MX": "Mexico",
-    "MY": "Malaysia",
-    "MZ": "Mozambique",
-    "NA": "Namibia",
-    "NC": "New Caledonia",
-    "NE": "Niger",
-    "NF": "Norfolk Island",
-    "NG": "Nigeria",
-    "NI": "Nicaragua",
-    "NL": "Netherlands",
-    "NO": "Norway",
-    "NP": "Nepal",
-    "NR": "Nauru",
-    "NU": "Niue",
-    "NZ": "New Zealand",
-    "OM": "Oman",
-    "PA": "Panama",
-    "PE": "Peru",
-    "PF": "French Polynesia",
-    "PG": "Papua New Guinea",
-    "PH": "Philippines",
-    "PK": "Pakistan",
-    "PL": "Poland",
-    "PM": "Saint Pierre and Miquelon",
-    "PN": "Pitcairn Islands",
-    "PR": "Puerto Rico",
-    "PS": "Palestinian Territory, Occupied",
-    "PT": "Portugal",
-    "PW": "Palau",
-    "PY": "Paraguay",
-    "QA": "Qatar",
-    "RE": "Reunion",
-    "RO": "Romania",
-    "RU": "Russian Federation",
-    "RW": "Rwanda",
-    "SA": "Saudi Arabia",
-    "SB": "Solomon Islands",
-    "SC": "Seychelles",
-    "SD": "Sudan",
-    "SE": "Sweden",
-    "SG": "Singapore",
-    "SH": "Saint Helena",
-    "SI": "Slovenia",
-    "SJ": "Svalbard and Jan Mayen",
-    "SK": "Slovakia",
-    "SL": "Sierra Leone",
-    "SM": "San Marino",
-    "SN": "Senegal",
-    "SO": "Somalia",
-    "SR": "Suriname",
-    "ST": "Sao Tome and Principe",
-    "SV": "El Salvador",
-    "SY": "Syrian Arab Republic",
-    "SZ": "Swaziland",
-    "TC": "Turks and Caicos Islands",
-    "TD": "Chad",
-    "TF": "French Southern Territories",
-    "TG": "Togo",
-    "TH": "Thailand",
-    "TJ": "Tajikistan",
-    "TK": "Tokelau",
-    "TM": "Turkmenistan",
-    "TN": "Tunisia",
-    "TO": "Tonga",
-    "TP": "East Timor",
-    "TR": "Turkey",
-    "TT": "Trinidad and Tobago",
-    "TV": "Tuvalu",
-    "TW": "Taiwan",
-    "TZ": "Tanzania, United Republic of",
-    "UA": "Ukraine",
-    "UG": "Uganda",
-    "UM": "United States Minor Outlying Islands",
-    "US": "United States",
-    "UY": "Uruguay",
-    "UZ": "Uzbekistan",
-    "VA": "Holy See (Vatican City State)",
-    "VC": "Saint Vincent and the Grenadines",
-    "VE": "Venezuela",
-    "VG": "Virgin Islands, British",
-    "VI": "Virgin Islands, U.S.",
-    "VN": "Vietnam",
-    "VU": "Vanuatu",
-    "WF": "Wallis and Futuna",
-    "WS": "Samoa",
-    "YE": "Yemen",
-    "YT": "Mayotte",
-    "YU": "Yugoslavia",
-    "ZA": "South Africa",
-    "ZM": "Zambia",
-    "ZR": "Zaire",
-    "ZW": "Zimbabwe",
-    "A1": "Anonymous Proxy",
-    "A2": "Satellite Provider",
-    "O1": "Other",
-}
