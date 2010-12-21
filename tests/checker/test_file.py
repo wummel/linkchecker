wummel/linkchecker

commit 90e0f4e5cc99744299076e2dd02fd900d09b7d74

Detect filenames with spaces as internal links.

linkcheck/checker/fileurl.py
@@ -266,14 +266,6 @@ class FileUrl (urlbase.UrlBase):
         @return non-empty regex pattern or None
         @rtype String or None
         """
-        url = absolute_url(self.base_url, self.base_ref, self.parent_url)
-        if not url:
+        if not self.url:
             return None
-        parts = strformat.url_unicode_split(url)
-        path = urlutil.splitparams(parts[2])[0]
-        segments = path.split('/')
-        if not self.is_directory():
-            # cut off filename to have a directory
-            segments = segments[:-1]
-        path = "/".join(segments)
-        return "file://%s" % re.escape(path)
+        return re.escape(self.url)

tests/checker/test_file.py
@@ -129,3 +129,19 @@ class TestFile (LinkCheckTest):
             u"valid",
         ]
         self.direct(url, resultlines)
+
+    def test_good_dir_space (self):
+        url = u"file://%(curdir)s/%(datadir)s/a b/" % self.get_attrs()
+        nurl = self.norm(url)
+        resultlines = [
+            u"url %s" % url,
+            u"cache key %s" % nurl,
+            u"real url %s" % nurl,
+            u"valid",
+            u"url t.txt",
+            u"cache key %st.txt" % nurl,
+            u"real url %st.txt" % nurl,
+            u"name t.txt",
+            u"valid",
+        ]
+        self.direct(url, resultlines, recursionlevel=1)

commit 0d8a583e39eff16fe30cc44351edb62dbb67ede0

Fix internal pattern for file URLs (regression from commit 90e0f4e)

linkcheck/checker/fileurl.py
@@ -266,6 +266,12 @@ class FileUrl (urlbase.UrlBase):
         @return non-empty regex pattern or None
         @rtype String or None
         """
-        if not self.url:
+        url = self.url
+        if not url:
             return None
-        return re.escape(self.url)
+        if url.startswith('file://'):
+            i = url.rindex('/')
+            if i > 6:
+                # remove last filename to make directory internal
+                url = url[:i+1]
+        return re.escape(url)

tests/checker/data/a b/bl.html
@@ -1,0 +1,3 @@
+<a href="bl.html#bl">Broken link</a>
+<a name="BL"><em>Broken link target</em>
+<a href="el.html">External link</a>

tests/checker/data/a b/el.html
@@ -1,0 +1,2 @@
+<a href="#bl">Broken link</a>
+<a name="BL"><em>Broken link target</em>

tests/checker/test_file.py
@@ -131,17 +131,31 @@ class TestFile (LinkCheckTest):
         self.direct(url, resultlines)
 
     def test_good_dir_space (self):
-        url = u"file://%(curdir)s/%(datadir)s/a b/" % self.get_attrs()
+        url = u"file://%(curdir)s/%(datadir)s/a b/bl.html" % self.get_attrs()
         nurl = self.norm(url)
+        url2 = u"file://%(curdir)s/%(datadir)s/a b/el.html" % self.get_attrs()
+        nurl2 = self.norm(url2)
         resultlines = [
             u"url %s" % url,
             u"cache key %s" % nurl,
             u"real url %s" % nurl,
             u"valid",
-            u"url t.txt",
-            u"cache key %st.txt" % nurl,
-            u"real url %st.txt" % nurl,
-            u"name t.txt",
+            u"url bl.html#bl",
+            u"cache key %s" % nurl,
+            u"real url %s" % nurl,
+            u"name Broken link",
+            u"warning Anchor `bl' not found. Available anchors: `BL'.",
+            u"valid",
+            u"url el.html",
+            u"cache key %s" % nurl2,
+            u"real url %s" % nurl2,
+            u"name External link",
+            u"valid",
+            u"url #bl",
+            u"cache key %s" % nurl2,
+            u"real url %s" % nurl2,
+            u"name Broken link",
+            u"warning Anchor `bl' not found. Available anchors: `BL'.",
             u"valid",
         ]
-        self.direct(url, resultlines, recursionlevel=1)
+        self.direct(url, resultlines, recursionlevel=2)