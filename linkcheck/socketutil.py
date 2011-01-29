8473bfee96484cdcfa02b84aa6dfe314afc32104
Bastian Kleineidam <calvin@debian.org>
2011-01-29 11:35:06 +0100

    Properly detect missing IPv6 protocol support.

--- linkcheck/socketutil.py
+++ linkcheck/socketutil.py
@@ -30,7 +30,8 @@ if socket.has_ipv6:
         # only catch these one:
         # socket.error: (97, 'Address family not supported by protocol')
         # socket.error: (10047, 'Address family not supported by protocol')
-        if msg.args[0] not in (97, 10047):
+        # socket.error: (43, 'Protocol not supported')
+        if msg.args[0] not in (97, 10047, 43):
             raise
 
 
