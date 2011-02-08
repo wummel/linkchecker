bb6797254de0db0c75a0078a7fad456ce3235210
Bastian Kleineidam <calvin@debian.org>
2011-02-08 21:28:15 +0100

    Bump up version.

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ except ImportError:
     pass
 from distutils.core import Distribution
 
-AppVersion = "6.3"
+AppVersion = "6.4"
 AppName = "LinkChecker"
 
 # py2exe options for windows .exe packaging
