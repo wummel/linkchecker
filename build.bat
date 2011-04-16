1cfc38eb4117b7e779b8a62f68707f51216c2860
Bastian Kleineidam <calvin@debian.org>
2011-04-16 17:30:51 +0200

    Add 64bit support.

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 :: Build LinkChecker
-:: Copyright (C) 2010 Bastian Kleineidam
+:: Copyright (C) 2010-2011 Bastian Kleineidam
 :: This program is free software; you can redistribute it and/or modify
 :: it under the terms of the GNU General Public License as published by
 :: the Free Software Foundation; either version 2 of the License, or
@@ -15,8 +15,16 @@
 :: 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 @echo off
 set PYDIR=C:\Python27
+set PYVER=2.7
+set PLATNAME=win32
+set COMPILER="-c mingw32"
+:: for 64bit platforms use Win SDK
+::set PLATNAME=win-amd64
+::set COMPILER=
 %PYDIR%\python.exe setup.py sdist --manifest-only
-%PYDIR%\python.exe setup.py build -c mingw32
-copy build\lib.win32-2.7\linkcheck\HtmlParser\htmlsax.pyd linkcheck\HtmlParser
-copy build\lib.win32-2.7\linkcheck\network\_network.pyd linkcheck\network
+%PYDIR%\python.exe setup.py build %COMPILER%
+:: copy .pyd files to start linkchecker in local directory
+copy build\lib.%PLATNAME%-%PYVER%\linkcheck\HtmlParser\htmlsax.pyd linkcheck\HtmlParser
+copy build\lib.%PLATNAME%-%PYVER%\linkcheck\network\_network.pyd linkcheck\network
+:: generate GUI documentation
 c:\qt\2010.05\qt\bin\qcollectiongenerator doc\html\lccollection.qhcp -o doc\html\lccollection.qhc
