442c0e8c061eac42077c032e7f6216bb2222236b
Bastian Kleineidam <calvin@debian.org>
2011-04-19 08:48:29 +0200

    Added documentation.

--- build.bat
+++ build.bat
@@ -14,17 +14,32 @@
 :: with this program; if not, write to the Free Software Foundation, Inc.,
 :: 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 @echo off
+:: Python version
 set PYDIR=C:\Python27
 set PYVER=2.7
+
+:: the compiler platform
 set PLATNAME=win32
-set COMPILER="-c mingw32"
-:: for 64bit platforms use Win SDK
+:: for 64bit platforms, see also
+:: http://wiki.cython.org/64BitCythonExtensionsOnWindows
 ::set PLATNAME=win-amd64
-::set COMPILER=
+
+:: the compiler
+::   MinGW (use only for 32bit platforms)
+::set COMPILER="-c mingw32"
+::   Microsoft SDK
+set COMPILER=
+set DISTUTILS_USE_SDK=1
+
+:: Qt SDK installation
+set QTDEV=c:\qt\2010.05\qt
+
+:: END configuration, no need to change anything below
+
 %PYDIR%\python.exe setup.py sdist --manifest-only
 %PYDIR%\python.exe setup.py build %COMPILER%
 :: copy .pyd files to start linkchecker in local directory
 copy build\lib.%PLATNAME%-%PYVER%\linkcheck\HtmlParser\htmlsax.pyd linkcheck\HtmlParser
 copy build\lib.%PLATNAME%-%PYVER%\linkcheck\network\_network.pyd linkcheck\network
 :: generate GUI documentation
-c:\qt\2010.05\qt\bin\qcollectiongenerator doc\html\lccollection.qhcp -o doc\html\lccollection.qhc
+%QTDEV%\bin\qcollectiongenerator doc\html\lccollection.qhcp -o doc\html\lccollection.qhc
