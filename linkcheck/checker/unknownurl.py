653b5f27dd92852e5258505aff987a1331efca24
Bastian Kleineidam <calvin@debian.org>
2012-09-22 16:18:37 +0200

    Updated ignored schemes.

--- linkcheck/checker/unknownurl.py
+++ linkcheck/checker/unknownurl.py
@@ -22,48 +22,115 @@ import re
 from . import urlbase
 from .const import WARN_IGNORE_URL
 
-ignored_schemes = r"""^(
-acap        # application configuration access protocol
-|afs        # Andrew File System global file names
-|chrome     # Mozilla specific
+# from http://www.iana.org/assignments/uri-schemes.html
+ignored_schemes_permanent = r"""
+aaas?       # Diameter Protocol
+|about      # about
+|acap       # application configuration access protocol
+|cap        # Calendar Access Protocol
 |cid        # content identifier
-|clsid      # Microsoft specific
+|crid       # TV-Anytime Content Reference Identifier
 |data       # data
 |dav        # dav
-|fax        # fax
-|feed       # RSS or Atom feeds
-|find       # Mozilla specific
+|dict       # dictionary service protocol
+|dns        # Domain Name System
+|geo        # Geographic Locations
+|go         # go
 |gopher     # Gopher
+|h323       # H.323
+|iax        # Inter-Asterisk eXchange Version 2
+|icap       # Internet Content Adaptation Protocol
+|im         # Instant Messaging
 |imap       # internet message access protocol
-|ircs?      # internet relay chat
-|isbn       # ISBN (int. book numbers)
-|javascript # JavaScript
+|info       # Information Assets with Identifiers in Public Namespaces
+|ipp        # Internet Printing Protocol
+|iris       # Internet Registry Information Service
+|iris\.(beep|xpcs?|lwz) #
 |ldap       # Lightweight Directory Access Protocol
-|mailserver # Access to data available from mail servers
 |mid        # message identifier
-|mms        # multimedia stream
-|modem      # modem
+|msrps?     # Message Session Relay Protocol
+|mtqp       # Message Tracking Query Protocol
+|mupdate    # Mailbox Update (MUPDATE) Protocol
 |nfs        # network file system protocol
+|nih?       #
 |opaquelocktoken # opaquelocktoken
 |pop        # Post Office Protocol v3
-|prospero   # Prospero Directory Service
-|rsync      # rsync protocol
+|pres       # Presence
 |rtsp       # real time streaming protocol
-|rtspu      # real time streaming protocol
 |service    # service location
+|session    #
 |shttp      # secure HTTP
-|sip        # session initiation protocol
-|skype      # Skype
+|sieve      # ManageSieve Protocol
+|sips?      # session initiation protocol
+|sms        # Short Message Service
+|snmp       # Simple Network Management Protocol
+|soap\.beeps? #
 |steam      # Steam browser protocol
+|tag        #
 |tel        # telephone
+|tftp       # Trivial File Transfer Protocol
+|thismessage #
 |tip        # Transaction Internet Protocol
 |tn3270     # Interactive 3270 emulation sessions
+|tv         # TV Broadcasts
+|urn        # Uniform Resource Names
 |vemmi      # versatile multimedia interface
-|wais       # Wide Area Information Servers
+|wss?       # WebSocket connections
+|xcon(-userid)? #
+|xmlrpc\.beeps? #
+|xmpp       #
 |z39\.50r   # Z39.50 Retrieval
 |z39\.50s   # Z39.50 Session
-):"""
+"""
 
+ignored_schemes_provisional = r"""
+|afs        # Andrew File System global file names
+|callto     #
+|com-eventbrite-attendee #
+|dlna-play(single|container) #
+|dtn        # DTNRG research and development
+|dvb        #
+|hcp        #
+|icon       #
+|ipn        #
+|jms        # Java Message Service
+|mms        # multimedia stream
+|ms-help    #
+|msnim      #
+|oid        #
+|res        #
+|rsync      # rsync protocol
+|skype      # Skype
+|view-source #
+"""
+
+ignored_schemes_historical = r"""
+|fax        # fax
+|mailserver # Access to data available from mail servers
+|modem      # modem
+|prospero   # Prospero Directory Service
+|videotex   #
+|wais       # Wide Area Information Servers
+|z39\.50    # Z39.50 information access
+"""
+
+ignored_schemes_other = r"""
+|chrome     # Mozilla specific
+|clsid      # Microsoft specific
+|feed       # RSS or Atom feeds
+|find       # Mozilla specific
+|isbn       # ISBN (int. book numbers)
+|ircs?      # internet relay chat
+|javascript # JavaScript
+"""
+
+
+ignored_schemes = "^(%s%s%s%s):" % (
+    ignored_schemes_permanent,
+    ignored_schemes_provisional,
+    ignored_schemes_historical,
+    ignored_schemes_other,
+)
 ignored_schemes_re = re.compile(ignored_schemes, re.VERBOSE)
 
 is_unknown_url = ignored_schemes_re.search
