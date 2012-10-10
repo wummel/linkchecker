57c2fd7b227ff73df630c36076666cda59dd9e15
Bastian Kleineidam <calvin@debian.org>
2012-10-10 21:05:07 +0200

    Add alexa run debug flag.

--- tests/run_alexa_1m.sh
+++ tests/run_alexa_1m.sh
@@ -10,11 +10,15 @@
 # Note that the result can depend on the current location.
 # Some sites have geo-location-aware content.
 set -u
+# always use default language
 LANG=C
+# for debugging
+#DEBUG=-Dall
+DEBUG=
 logfile=alexa_1m.log
 errfile=alexa_1m_err.log
 rm -f $logfile $errfile
 for url in $(shuf $HOME/src/alexatopsites/top-1m.txt); do
   echo "Checking $url" | tee -a $logfile | tee -a $errfile
-  ./linkchecker -r1 --no-status -Dall $url >> $logfile 2>>$errfile
+  ./linkchecker -r1 --no-status $DEBUG $url >> $logfile 2>>$errfile
 done
