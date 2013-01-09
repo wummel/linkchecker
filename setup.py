7fe72745aeba4df053cfdff1f20aebc250e3f1eb
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-01-09 23:03:12 +0100

    Updated copyright.

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: iso-8859-1 -*-
-# Copyright (C) 2000-2012 Bastian Kleineidam
+# Copyright (C) 2000-2013 Bastian Kleineidam
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
