c676a4c829ee31857485400e106b5d623610029d
Bastian Kleineidam <bastian.kleineidam@web.de>
2013-11-30 22:07:23 +0100

    Avoid DoS in SSL certificate host matching.

--- linkcheck/checker/httpsurl.py
+++ linkcheck/checker/httpsurl.py
@@ -118,10 +118,17 @@ class CertificateError(ValueError):
     pass
 
 
-def _dnsname_to_pat(dn):
+def _dnsname_to_pat(dn, max_wildcards=1):
     """Convert a DNS certificate name to a hostname matcher."""
     pats = []
     for frag in dn.split(r'.'):
+        if frag.count('*') > max_wildcards:
+            # Issue #17980: avoid denials of service by refusing more
+            # than one wildcard per fragment.  A survery of established
+            # policy among SSL implementations showed it to be a
+            # reasonable choice.
+            raise CertificateError(
+                "too many wildcards in certificate DNS name: " + repr(dn))
         if frag == '*':
             # When '*' is a fragment by itself, it matches a non-empty dotless
             # fragment.
