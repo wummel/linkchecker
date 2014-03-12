279db5c5b82a04ffde1c1f7de9805b9926cc385a
Bastian Kleineidam <bastian.kleineidam@web.de>
2014-03-12 19:22:18 +0100

    Fix documentation.

--- linkcheck/plugins/anchorcheck.py
+++ linkcheck/plugins/anchorcheck.py
@@ -15,7 +15,7 @@
 # with this program; if not, write to the Free Software Foundation, Inc.,
 # 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 """
-Check page content for virus infection with clamav.
+Check HTML anchors
 """
 from . import _ContentPlugin
 from .. import log, LOG_PLUGIN, url as urlutil
